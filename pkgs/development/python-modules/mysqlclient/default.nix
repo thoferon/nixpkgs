fc8b36898cadf5442fe96ac4471148a63bd5914e
Frederik Rietdijk <fridh@fridh.nl>
2018-12-19 09:44:09 +0100

    python: mysqlclient: 1.3.13 -> 1.3.14

--- pkgs/development/python-modules/mysqlclient/default.nix
+++ pkgs/development/python-modules/mysqlclient/default.nix
@@ -2,7 +2,7 @@
 
 buildPythonPackage rec {
   pname = "mysqlclient";
-  version = "1.3.13";
+  version = "1.3.14";
 
   buildInputs = [
     mysql.connector-c
@@ -13,7 +13,7 @@ buildPythonPackage rec {
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "ff8ee1be84215e6c30a746b728c41eb0701a46ca76e343af445b35ce6250644f";
+    sha256 = "3981ae9ce545901a36a8b7aed76ed02960a429f75dc53b7ad77fb2f9ab7cd56b";
   };
 
   meta = with stdenv.lib; {
