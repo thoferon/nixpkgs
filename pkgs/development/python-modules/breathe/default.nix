dde641b4f44cdf827b28de128dd31755459d5af0
Frederik Rietdijk <fridh@fridh.nl>
2018-12-19 09:44:06 +0100

    python: breathe: 4.11.0 -> 4.11.1

--- pkgs/development/python-modules/breathe/default.nix
+++ pkgs/development/python-modules/breathe/default.nix
@@ -1,12 +1,12 @@
 { lib, fetchPypi, buildPythonPackage, docutils, six, sphinx, isPy3k }:
 
 buildPythonPackage rec {
-  version = "4.11.0";
+  version = "4.11.1";
   pname = "breathe";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "05x3qrvsriy0cn0p4bxnzhp27pvxbq2vxlxncr2wqh003gpbp4fa";
+    sha256 = "9b7a94122039ad61383551a696d9c1fa5c16b423a28dadf113389a481d03fad6";
   };
 
   propagatedBuildInputs = [ docutils six sphinx ];
