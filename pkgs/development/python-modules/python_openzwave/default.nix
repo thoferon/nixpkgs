def67e4a60692882e2ce411373134800097e72e8
Frederik Rietdijk <fridh@fridh.nl>
2018-12-19 09:44:11 +0100

    python: python_openzwave: 0.4.11 -> 0.4.16

--- pkgs/development/python-modules/python_openzwave/default.nix
+++ pkgs/development/python-modules/python_openzwave/default.nix
@@ -5,13 +5,13 @@
 
 buildPythonPackage rec {
   pname = "python_openzwave";
-  version = "0.4.11";
+  version = "0.4.16";
 
   disabled = !isPy3k;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "2464c364929acaee5da39eda60976a3efa40bb1f7a5d9362531d53cfa5c3bd09";
+    sha256 = "7f699da1a387a713d5bebbba1d03bac437a633a7a85138f61e806c000782bad5";
     extension = "zip";
   };
 
